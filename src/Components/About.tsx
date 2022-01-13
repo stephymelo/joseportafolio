04ef4a76a95ef3eadadca2322bc28b2d6199fe85
stephym <stephymelo99@gmail.com>
2022-01-12 23:29:57 -0500

    fixes

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -5,9 +5,15 @@ interface About {
     type: 'english' | 'spanish';
 }
 
-export const About: React.FC<About> = ({type}) => {
+export const About: React.FC<About> = ({ type }) => {
     return (
         <section id="about" className='about'>
+
+            <svg className='about__decor' width="450" height="250" viewBox="0 0 596 302" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M548.562 218.635C677.283 295.668 498.513 288.5 393.039 275.288C368.853 290.822 275.295 298.307 94.5452 203.972C-131.392 86.0543 114.648 -48.0397 253.358 49.0316C392.067 146.103 387.661 122.344 548.562 218.635Z" stroke="#1F2D7C" strokeWidth="4" />
+                <path d="M499.209 198.786C611.008 261.505 455.556 255.977 363.855 245.373C342.794 258.113 261.418 264.403 104.404 187.639C-91.8628 91.6851 122.351 -18.3595 242.8 60.7182C363.248 139.796 359.46 120.387 499.209 198.786Z" stroke="#1F2D7C" strokeWidth="4" />
+            </svg>
+
 
 
 
@@ -22,11 +28,12 @@ export const About: React.FC<About> = ({type}) => {
 
                 <h3>José Miguel Tamayo, Colombian</h3>
                 <div className='about__info__bio'>
-                    <p>I have more than 11 years of experience as a solo singer and as member of choral ensembles, participating in events such as the Bandola festival in Sevilla, “Antioquia le canta a Colombia” festival, the Javeriana Choir Festival, among others.
+                    <p>I am a musician and producer from ICESI University of Cali, Colombia.
+                        I have more than 11 years of experience as a solo singer and as member of choral ensembles. I have also worked as an assistant in studio recordings, and I am currently projecting my career as a composer in the world of audio for audiovisual and interactive media.
                     </p>
                 </div>
 
-            </article> }
+            </article>}
 
             {type === 'spanish' && <article className="about__info">
                 <div className='about__info__title'>
@@ -40,18 +47,13 @@ export const About: React.FC<About> = ({type}) => {
                 <h3>José Miguel Tamayo, Colombiano</h3>
                 <div className='about__info__bio'>
                     <p>Soy un músico y productor de la universidad ICESI de Cali, Colombia.
-Tengo más de 11 años de experiencia como cantante solista e integrante de agrupaciones corales. He trabajado como asistente en grabaciones de estudio, y actualmente proyecto mi carrera como compositor en el mundo del sonido para medios audiovisuales e interactivos.
+                        Tengo más de 11 años de experiencia como cantante solista e integrante de agrupaciones corales. He trabajado como asistente en grabaciones de estudio, y actualmente proyecto mi carrera como compositor en el mundo del sonido para medios audiovisuales e interactivos.
                     </p>
                 </div>
 
-            </article> }
-            
+            </article>}
 
 
-            <svg className='about__decor' width="450" height="250" viewBox="0 0 596 302" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M548.562 218.635C677.283 295.668 498.513 288.5 393.039 275.288C368.853 290.822 275.295 298.307 94.5452 203.972C-131.392 86.0543 114.648 -48.0397 253.358 49.0316C392.067 146.103 387.661 122.344 548.562 218.635Z" stroke="#1F2D7C" strokeWidth="4" />
-                <path d="M499.209 198.786C611.008 261.505 455.556 255.977 363.855 245.373C342.794 258.113 261.418 264.403 104.404 187.639C-91.8628 91.6851 122.351 -18.3595 242.8 60.7182C363.248 139.796 359.46 120.387 499.209 198.786Z" stroke="#1F2D7C" strokeWidth="4" />
-            </svg>
 
             <img className="about__photo" src={jose} />
 
--- src/Components/Navigation.tsx
+++ src/Components/Navigation.tsx
@@ -1,145 +1,311 @@
 import * as React from 'react';
+import { useEffect } from 'react';
 import Scroll, { Link } from 'react-scroll';
 const ScrollLink = Scroll.ScrollLink;
 
+
 interface Navigation {
     type: 'english' | 'spanish';
+
+    
 }
 
-export const Navigation: React.FC<Navigation> = ({type}) => {
+export const Navigation: React.FC<Navigation> = ({ type }) => {
+    //  const state = {isToggleOn: true};
+    //  const [ state, setState ] = React.useState< {isToggleOn: true}|{isToggleOn: false}>();
+    const [state, setState] = React.useState(false)
+
+    const handleBurgerView =()=>{
+        setState(current => !current)
+    }
+
+    useEffect( () => {
+    }, [state]);
+
     return (
         <div className='Navigation'>
 
-            {type==='english' && <ul>
-                <li>
-                    <Link
-                        to="banner"
-                        spy={true}
-                        smooth={true}
-                        duration={200}
-                        className='some-class'
-                        activeClass='some-active-class'
-                    >
-                        HOME
-                    </Link>
-                </li>
-                <li>
-                    <Link
-                        to="about"
-                        spy={true}
-                        smooth={true}
-                        duration={200}
-                        className='some-class'
-                        activeClass='some-active-class'
-                    >
-                        ABOUT
-                    </Link>
-                </li>
-                <li>
-                    <Link
-                        to="work"
-                        spy={true}
-                        smooth={true}
-                        duration={500}
-                        className='some-class'
-                        activeClass='some-active-class'
-                    >
-                        WORK
-                    </Link>
-                </li>
+            <nav className='normalNav'>
 
-                <li>
-                    <Link
-                        to="gallery"
-                        spy={true}
-                        smooth={true}
-                        duration={500}
-                        className='some-class'
-                        activeClass='some-active-class'
-                    >
-                        GALLERY
-                    </Link>
-                </li>
+                {type === 'english' && <ul className='ulNav'>
+                    <li>
+                        <Link
+                            to="banner"
+                            spy={true}
+                            smooth={true}
+                            duration={200}
+                            className='some-class'
+                            activeClass='some-active-class'
+                        >
+                            HOME
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            to="about"
+                            spy={true}
+                            smooth={true}
+                            duration={200}
+                            className='some-class'
+                            activeClass='some-active-class'
+                        >
+                            ABOUT
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            to="work"
+                            spy={true}
+                            smooth={true}
+                            duration={500}
+                            className='some-class'
+                            activeClass='some-active-class'
+                        >
+                            WORK
+                        </Link>
+                    </li>
 
-                <li>
-                    <Link
-                        to="contact"
-                        spy={true}
-                        smooth={true}
-                        duration={500}
-                        className='some-class'
-                        activeClass='some-active-class'
-                    >
-                        CONTACT
-                    </Link>
-                </li>
-            </ul>}
+                    <li>
+                        <Link
+                            to="gallery"
+                            spy={true}
+                            smooth={true}
+                            duration={500}
+                            className='some-class'
+                            activeClass='some-active-class'
+                        >
+                            GALLERY
+                        </Link>
+                    </li>
 
-            {type==='spanish' && <ul>
-                <li>
-                    <Link
-                        to="banner"
-                        spy={true}
-                        smooth={true}
-                        duration={200}
-                        className='some-class'
-                        activeClass='some-active-class'
-                    >
-                        HOME
-                    </Link>
-                </li>
-                <li>
-                    <Link
-                        to="about"
-                        spy={true}
-                        smooth={true}
-                        duration={200}
-                        className='some-class'
-                        activeClass='some-active-class'
-                    >
-                        SOBRE MI
-                    </Link>
-                </li>
-                <li>
-                    <Link
-                        to="work"
-                        spy={true}
-                        smooth={true}
-                        duration={500}
-                        className='some-class'
-                        activeClass='some-active-class'
-                    >
-                        TRABAJOS
-                    </Link>
-                </li>
+                    <li>
+                        <Link
+                            to="contact"
+                            spy={true}
+                            smooth={true}
+                            duration={500}
+                            className='some-class'
+                            activeClass='some-active-class'
+                        >
+                            CONTACT
+                        </Link>
+                    </li>
+                </ul>}
 
-                <li>
-                    <Link
-                        to="gallery"
-                        spy={true}
-                        smooth={true}
-                        duration={500}
-                        className='some-class'
-                        activeClass='some-active-class'
-                    >
-                        GALLERIA
-                    </Link>
-                </li>
+                {type === 'spanish' && <ul className='ulNav'>
+                    <li>
+                        <Link
+                            to="banner"
+                            spy={true}
+                            smooth={true}
+                            duration={200}
+                            className='some-class'
+                            activeClass='some-active-class'
+                        >
+                            HOME
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            to="about"
+                            spy={true}
+                            smooth={true}
+                            duration={200}
+                            className='some-class'
+                            activeClass='some-active-class'
+                        >
+                            SOBRE MI
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            to="work"
+                            spy={true}
+                            smooth={true}
+                            duration={500}
+                            className='some-class'
+                            activeClass='some-active-class'
+                        >
+                            TRABAJOS
+                        </Link>
+                    </li>
 
-                <li>
-                    <Link
-                        to="contact"
-                        spy={true}
-                        smooth={true}
-                        duration={500}
-                        className='some-class'
-                        activeClass='some-active-class'
-                    >
-                        CONTACTO
-                    </Link>
-                </li>
-            </ul>}
+                    <li>
+                        <Link
+                            to="gallery"
+                            spy={true}
+                            smooth={true}
+                            duration={500}
+                            className='some-class'
+                            activeClass='some-active-class'
+                        >
+                            GALLERIA
+                        </Link>
+                    </li>
+
+                    <li>
+                        <Link
+                            to="contact"
+                            spy={true}
+                            smooth={true}
+                            duration={500}
+                            className='some-class'
+                            activeClass='some-active-class'
+                        >
+                            CONTACTO
+                        </Link>
+                    </li>
+                </ul>}
+            </nav>
+
+
+            {/* ////// */}
+
+
+
+            <div onClick={handleBurgerView} className="burgerMenu">
+                <hr></hr>
+                <hr></hr>
+                <hr></hr>
+            </div>
+
+            {state ? <nav className='burgerNavigation'>
+
+
+{type === 'english' && <ul className='ulBurger'>
+    <li>
+        <Link
+            to="banner"
+            spy={true}
+            smooth={true}
+            duration={200}
+            className='some-class'
+            activeClass='some-active-class'
+        >
+            HOME
+        </Link>
+    </li>
+    <li>
+        <Link
+            to="about"
+            spy={true}
+            smooth={true}
+            duration={200}
+            className='some-class'
+            activeClass='some-active-class'
+        >
+            ABOUT
+        </Link>
+    </li>
+    <li>
+        <Link
+            to="work"
+            spy={true}
+            smooth={true}
+            duration={500}
+            className='some-class'
+            activeClass='some-active-class'
+        >
+            WORK
+        </Link>
+    </li>
+
+    <li>
+        <Link
+            to="gallery"
+            spy={true}
+            smooth={true}
+            duration={500}
+            className='some-class'
+            activeClass='some-active-class'
+        >
+            GALLERY
+        </Link>
+    </li>
+
+    <li>
+        <Link
+            to="contact"
+            spy={true}
+            smooth={true}
+            duration={500}
+            className='some-class'
+            activeClass='some-active-class'
+        >
+            CONTACT
+        </Link>
+    </li>
+</ul>}
+
+{type === 'spanish' && <ul className='ulBurger'>
+    <li>
+        <Link
+            to="banner"
+            spy={true}
+            smooth={true}
+            duration={200}
+            className='some-class'
+            activeClass='some-active-class'
+        >
+            HOME
+        </Link>
+    </li>
+    <li>
+        <Link
+            to="about"
+            spy={true}
+            smooth={true}
+            duration={200}
+            className='some-class'
+            activeClass='some-active-class'
+        >
+            SOBRE MI
+        </Link>
+    </li>
+    <li>
+        <Link
+            to="work"
+            spy={true}
+            smooth={true}
+            duration={500}
+            className='some-class'
+            activeClass='some-active-class'
+        >
+            TRABAJOS
+        </Link>
+    </li>
+
+    <li>
+        <Link
+            to="gallery"
+            spy={true}
+            smooth={true}
+            duration={500}
+            className='some-class'
+            activeClass='some-active-class'
+        >
+            GALLERIA
+        </Link>
+    </li>
+
+    <li>
+        <Link
+            to="contact"
+            spy={true}
+            smooth={true}
+            duration={500}
+            className='some-class'
+            activeClass='some-active-class'
+        >
+            CONTACTO
+        </Link>
+    </li>
+</ul>}
+
+</nav> : ' ' }
+
 
         </div>
+
     );
 }
